0d231d3e1e3a17399788e18756d7a3eed1abdf33
Marty Germane (WSL 2) <cr9sarthakavlash@gmail.com>
2021-08-18 17:04:43 +0530

    Added buttons and UI

--- script.js
+++ script.js
@@ -2,27 +2,82 @@ let rock = 'Rock';
 let paper = 'Paper';
 let scissors = 'Scissors';
 
-let gameArray = [rock, paper, scissors];
+let choices = [rock, paper, scissors];
 
-let playerScore = 0;
 let compScore = 0;
+let playerScore = 0;
 
-randomFunc = (numArray) => {
-    let random1 = Math.random() * numArray.length;
-    let random2 = Math.floor(random1);
-    return random2;
+function randomFunc(arrLength) {
+    let step1 = (Math.random() * 10) * arrLength / 10;
+    // console.log(step1);
+
+    let step2 = Math.floor(step1);
+    // console.log(step2);
+
+    return step2;
+
 }
 
-singleRound = (playerSelection, computerSelection) => {
+function caseCorrection(str) {
+    let step1 = str.charAt(0).toUpperCase();
+    let step2 = str.slice(1).toLowerCase();
+    return step1 + step2;
+}
+
+computerPlay = () => {
+    let computerChoice = randomFunc(choices.length);
+    return caseCorrection(choices[computerChoice]);
+
+}
+
+// console.log(computerPlay());
+
+// Checking who is the winner of the game!
+function winner()
+{
+    if (playerScore > compScore)
+        return 'Player wins!';
+    else if (compScore > playerScore)
+        return 'Computer wins!';
+    else if (playerScore == compScore)
+        return 'It was a draw';
+    else
+        return 'Something wrong happened!';
+}
+
+// To check if the player or computer has reached a count of 5 and the game stops.
+function gameEnd()
+{
+    if (playerScore == 5 || compScore == 5) {
+        divResult.textContent = ('[Final] Player Score: ' + playerScore + ', Computer Score: ' + compScore);
+        winner();
+        button.removeEventListener('click', (e) => {});
+        return;
+    }
+}
+
+playRound = (computerSelection, playerSelection) => {
+    
+    computerSelection = computerPlay();
+    computerSelection;
+
+    // playerSelection = caseCorrection(prompt('Enter your hand!'));
+    playerSelection;
+
+    gameEnd();
+
     if (playerSelection == rock && computerSelection == paper
         || playerSelection == paper && computerSelection == scissors
         || playerSelection == scissors && computerSelection == rock) {
         compScore++;
-        return 'You Lose! ' + playerSelection + ' beats ' + computerSelection;
+        gameEnd();
+        return 'You Lose! ' + computerSelection + ' beats ' + playerSelection;
     }
-
-    else if (computerSelection == rock && playerSelection == paper || computerSelection == paper && playerSelection == scissors || computerSelection == scissors && playerSelection == rock) {
+    else if (computerSelection == rock && playerSelection == paper
+        || computerSelection == paper && playerSelection == scissors
+        || computerSelection == scissors && playerSelection == rock) {
         playerScore++;
+        gameEnd();
         return 'You win! ' + playerSelection + ' beats ' + computerSelection;
     }
     else if (playerSelection == computerSelection) {
@@ -32,21 +87,22 @@ singleRound = (playerSelection, computerSelection) => {
         return 'You entered a wrong value!';
 }
 
-caseCorrection = (str) => {
-    let result = str.charAt(0).toUpperCase() + str.substr(1);
-    return result;
-}
 
-function computerPlay() {
-    console.log('-----------------------------------------------');
-    for (i = 0; i < 5; i++) {
-        let store = randomFunc(gameArray);
-        let storePlayer = caseCorrection(prompt('Enter your hand!'));
 
-        console.log(singleRound(storePlayer, gameArray[store]));
-    }
-    console.log('Player Score: ' + playerScore + ', Computer Score: ' + compScore);
-    console.log('-----------------------------------------------');
-}
+// console.log(playRound());
 
-computerPlay();
+let buttons = document.querySelectorAll('button');
+let divWhoBeatWho = document.querySelector('.whoBeatWho');
+let divResult = document.querySelector('.result');
+
+// Defining computer's selection for future use!
+let computerSelection;
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        divWhoBeatWho.textContent = playRound(computerSelection, caseCorrection(button.value));
+        divResult.textContent = ('Player Score: ' + playerScore + ', Computer Score: ' + compScore);
+    });
+});
+
+
+
